546a70b689e8f362137eb07d983f259ce4cd4d48
Rokibul Islam <rokibul630@gmail.com>
2022-04-06 14:07:38 +0530

    add leftouts

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+
+        {{-- livewire style --}}
+        @livewireStyles
+      
+    </head>
+
+    <body>
+
+       <h4>Frontend page......</h4>
+      
+
+        {{-- livewire script --}}
+        @livewireScripts
+    </body>
+</html>
